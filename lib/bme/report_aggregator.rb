66e7f385a922affb5a755fde7aa0dbdf415fcea3
Lenny Marks <lenny@aps.org>
2013-08-27 12:22:08 -0400

    Better column labeling and formatting

--- lib/bme/report_aggregator.rb
+++ lib/bme/report_aggregator.rb
@@ -2,11 +2,12 @@ require 'easystats'
 
 module BME
   class ReportAggregator
-    attr_reader :labels, :keyed_reports, :total, :precision, :label_width
+    attr_reader :labels, :keyed_reports, :total, :precision, :label_width, :column_width
 
     def initialize(labels, options = {})
       @labels = labels
       @label_width = options.fetch(:label_width, 20)
+      @column_width = options.fetch(:column_width, 15)
       @precision = options.fetch(:precision, 5)
       @keyed_reports = {}
       @mutex = Mutex.new
@@ -17,15 +18,20 @@ module BME
 
       @mutex.synchronize do
         @total = total.nil? ? r : total + r
-        puts sprintf("%-#{label_width}s #{r}", label)
+        puts report_s(r, label)
         keys.each do |k|
           (keyed_reports[k] ||= []) << r
         end
       end
     end
 
+    def table_header
+      w = column_width
+      sprintf("%-#{label_width}s %-#{w}s %-#{w}s %-#{w}s %s", '', 'user', 'system', 'total', 'real')
+    end
+
     def stats
-      buf = sprintf("%-#{label_width}s #{total}", '>Total:')
+      buf = report_s(total, '>Total:')
       p = precision
       labels.each do |k|
         realtimes = keyed_reports.fetch(k).map(&:real)
@@ -33,6 +39,13 @@ module BME
                        "#{k}(#{realtimes.size})", realtimes.sum, realtimes.average, realtimes.median, realtimes.standard_deviation, realtimes.min, realtimes.max)
       end
       buf
+    end
+
+    private
+
+    def report_s(r, label)
+      w = column_width
+      sprintf("%-#{label_width}s #{r.format("%-#{w}u %-#{w}y %-#{w}t %r")}\n", label)
     end
   end
 end
